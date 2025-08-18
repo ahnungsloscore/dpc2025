701bc75801dad71c75b0ef6d7a5a2d1b98d16ca6
ahnungsloscore <pranjalsawant002@gmail.com>
2025-08-18 23:01:40 +0530

    solution.java

--- Day-04/solution.java
+++ Day-04/solution.java
@@ -6,22 +6,22 @@ public class mergeSortedArray {
         Scanner sc = new Scanner(System.in);
         // Input arr1
         System.out.print("Enter size of arr1: ");
-        int m = sc.nextInt();
-        int[] arr1 = new int[m];
+        int size1 = sc.nextInt();
+        int[] arr1 = new int[size1];
         System.out.println("Enter elements of arr1:");
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < size1; i++) {
             arr1[i] = sc.nextInt();
         }
         // Input arr2
         System.out.print("Enter size of arr2: ");
-        int n = sc.nextInt();
+        int size2 = sc.nextInt();
         int[] arr2 = new int[n];
         System.out.println("Enter elements of arr2:");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < size2; i++) {
             arr2[i] = sc.nextInt();
         }
         int i = 0, j = 0;
-        while (i < m && j < n) {
+        while (i < size1 && j < size2) {
             if (arr1[i] > arr2[j]) {
                 // Swap
                 int temp = arr1[i];
