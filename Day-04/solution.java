47e1c76b12484990c03406b816212cb4bbe3d172
ahnungsloscore <pranjalsawant002@gmail.com>
2025-08-18 22:42:05 +0530

    Create solution.java
    day4

---
+++ Day-04/solution.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class mergeSortedArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Input arr1
+        System.out.print("Enter size of arr1: ");
+        int m = sc.nextInt();
+        int[] arr1 = new int[m];
+        System.out.println("Enter elements of arr1:");
+        for (int i = 0; i < m; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        // Input arr2
+        System.out.print("Enter size of arr2: ");
+        int n = sc.nextInt();
+        int[] arr2 = new int[n];
+        System.out.println("Enter elements of arr2:");
+        for (int i = 0; i < n; i++) {
+            arr2[i] = sc.nextInt();
+        }
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (arr1[i] > arr2[j]) {
+                // Swap
+                int temp = arr1[i];
+                arr1[i] = arr2[j];
+                arr2[j] = temp;
+                Arrays.sort(arr2);
+            }
+            i++;
+        }
+        // Print result
+        System.out.print("Merged arr1: ");
+        for (int k = 0; k < arr1.length; k++) {
+            System.out.print(arr1[k] + " ");
+        }
+        System.out.println();
+        System.out.print("Merged arr2: ");
+        for (int k = 0; k < arr2.length; k++) {
+            System.out.print(arr2[k] + " ");
+        }
+        System.out.println();
+    }
+}
